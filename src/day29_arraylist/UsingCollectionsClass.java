2259a26a7164a6c2032a0328701cdb2bcaf9d09b
YuliiaHlinska <glinchiksony@gmail.com>
2024-12-27 22:09:43 -0500

    constructor_chaining

--- src/day29_arraylist/UsingCollectionsClass.java
+++ src/day29_arraylist/UsingCollectionsClass.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class UsingCollectionsClass {
+public class  UsingCollectionsClass {
     public static void main(String[] args) {
         // Collections ---- > 's' at the end
 
